7e605986051c2bc344aaf7f33c5795def78c2293
。 <146692624+mm7274a00@users.noreply.github.com>
2023-11-23 02:12:00 +0800

    1123 0211

--- src/test/java/com/example/questionnaire/QuizServiceTest.java
+++ src/test/java/com/example/questionnaire/QuizServiceTest.java
@@ -40,6 +40,17 @@ public class QuizServiceTest {
 	
     @Test
     public void updateTest() {
+    	Questionnaire questionnaire = new Questionnaire(25,"test1", "test",false, 
+				LocalDate.of(2023, 11, 17), LocalDate.of(2023, 11, 30));
+		
+		List<Question> questionList = new ArrayList<>();
+		Question q1 = new Question(1,"test_question_111","single",false,"AAA;BBB;CCC");
+		Question q2 = new Question(2,"test_question_2","multi",false,"10;20;30;40");
+		Question q3 = new Question(3,"test_question_3","text",false,"ABC");
+		questionList.addAll(Arrays.asList(q1, q2, q3));
+		QuizReq req = new QuizReq(questionnaire,questionList);
+        QuizRes updateRes = service.update(req);
+		Assert.isTrue(updateRes.getRtnCode().getCode() == 200, "update error!");
     }
      
     @Test
