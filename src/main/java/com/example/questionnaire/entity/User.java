d05abeaba070e5fa24616adbd747d99e90b18908
mm7274a00 <mm7274a00@gmail.com>
2023-11-23 17:53:55 +0800

    1123

--- src/main/java/com/example/questionnaire/entity/User.java
+++ src/main/java/com/example/questionnaire/entity/User.java
@@ -1,5 +1,121 @@
 package com.example.questionnaire.entity;
 
-public class User {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
+@Entity
+@Table(name = "user")
+public class User {
+	
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Id
+	@Column
+	private int num;
+	
+	@Column
+	private String name;
+	
+	@Column
+	private String phoneNumber;
+	
+	@Column
+	private String email;
+	
+	@Column
+	private String age;
+	
+	@Column
+	private int qnId;
+	
+	@Column
+	private int qId;
+	
+	@Column
+	private String ans;
+
+	public User() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public User(int num, String name, String phoneNumber, String email, String age, int qnId, int qId, String ans) {
+		super();
+		this.num = num;
+		this.name = name;
+		this.phoneNumber = phoneNumber;
+		this.email = email;
+		this.age = age;
+		this.qnId = qnId;
+		this.qId = qId;
+		this.ans = ans;
+	}
+
+	public int getNum() {
+		return num;
+	}
+
+	public void setNum(int num) {
+		this.num = num;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getAge() {
+		return age;
+	}
+
+	public void setAge(String age) {
+		this.age = age;
+	}
+
+	public int getQnId() {
+		return qnId;
+	}
+
+	public void setQnId(int qnId) {
+		this.qnId = qnId;
+	}
+
+	public int getqId() {
+		return qId;
+	}
+
+	public void setqId(int qId) {
+		this.qId = qId;
+	}
+
+	public String getAns() {
+		return ans;
+	}
+
+	public void setAns(String ans) {
+		this.ans = ans;
+	}
+	
 }
